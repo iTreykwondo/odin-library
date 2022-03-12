43b7ba2acab2afc372939e6e906ebf1eaacc770a
Trajan Miller <trajanmiller324@gmail.com>
2022-03-12 18:21:42 -0500

    Create the Book object and library list

---
+++ app.js
@@ -0,0 +1,8 @@
+let myLibrary = [];
+
+function Book(title, author, pageCount, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.readStatus = readStatus;
+}
